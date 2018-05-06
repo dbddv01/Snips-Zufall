32b0fc35d5e683e6790e8e40a09dab6e84ea2e81
dbddv01 <39023861+dbddv01@users.noreply.github.com>
2018-05-06 12:05:57 +0200

    Update action-domi-randomThing-Zubehoer.py

--- action-domi-randomThing-Zubehoer.py
+++ action-domi-randomThing-Zubehoer.py
@@ -39,25 +39,25 @@ def action_wrapper(hermes, intentMessage, conf):
     Refer to the documentation for further details. 
     """ 
     item = intentMessage.slots.item_random.first().value
-    if item == 'coin':
+    if item == 'monnaie':
         coin_random = random.randrange(0, 1)
         if coin_random == 0:
-            result_sentence = "Es ist ein Kopf."
+            result_sentence = "Le résultat est Pile"
         else:
-            result_sentence = "Es ist eine Zahl."
-    elif item == 'dice':
+            result_sentence = "Le résulat est face"
+    elif item == 'dés':
         dice_random = random.randrange(1, 6)
-        result_sentence = "Ich habe eine {number} gewürfelt.".format(number=dice_random)
-    elif item == 'number':
+        result_sentence = "Le dé tombe sur le numéro {number} ".format(number=dice_random)
+    elif item == 'nombre':
         number_random = random.randrange(0, 1000)
-        result_sentence = "Die {number} habe ich gerade zufällig gewählt.".format(number=number_random)
+        result_sentence = "Le nombre aléatoire généré est {number} .".format(number=number_random)
     else:
-        result_sentence = "Ich habe dich nicht verstanden."
+        result_sentence = "Désolé je n'ai pas compris. "
     current_session_id = intentMessage.session_id
     hermes.publish_end_session(current_session_id, result_sentence)
 
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("domi:randomThing", subscribe_intent_callback) \
+        h.subscribe_intent("Dbddv01:randomThing", subscribe_intent_callback) \
 .start()
